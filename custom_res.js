5bad1bc904881eae58a76478b8ba18795254bbfb
Arjun Reddy <141067703+arjunreddy20@users.noreply.github.com>
2024-11-13 13:36:33 +0530

    customized input

---
+++ custom_res.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    if (req.url === "/home"){
+        res.write("<body><h1> Welcome to home </h1></body>")
+    }
+    if (req.url === "/about"){
+        res.write("<body><h1> welcome to about page</h1></body>")
+    }
+
+    res.write("</html>");
+    res.end();
+
+});
+server.listen(3000)
