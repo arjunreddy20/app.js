c2a429d7f73988b970d6ac95956d6033068371f0
Arjun Reddy <141067703+arjunreddy20@users.noreply.github.com>
2024-11-13 13:40:11 +0530

    Update custom_res.js

--- custom_res.js
+++ custom_res.js
@@ -10,7 +10,8 @@ const server = http.createServer((req,res) => {
     if (req.url === "/about"){
         res.write("<body><h1> welcome to about page</h1></body>")
     }
-
+    if (req.url === "/node"){
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>")
     res.write("</html>");
     res.end();
 
